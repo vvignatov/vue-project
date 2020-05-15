3dd9ff16eb9293decb99e56f6080b996a5ecb31c
v_ignatov <inaburat@gmail.com>
2020-05-15 21:24:29 +0300

    vue-resource ande json-server is added

--- src/containers/vmodules/Vmodules.vue
+++ src/containers/vmodules/Vmodules.vue
@@ -1,11 +1,12 @@
 <template>
 	<div>
-		<root-component :title="'1. Vue-router'">
+		<root-component :title="'1. Vue-router (работа с ссылками)'">
 			<p>Динамические роуты, программная навигация, вложенные роуты, передача параметров, хэш, редирект, защита роутов(guards)</p>
 			<app-routes></app-routes>
 		</root-component>
 
-		<root-component :title="'2. Vue-resource'">
+		<root-component :title="'2. Vue-resource (работа с сервером)'">
+			<app-resource></app-resource>
 		</root-component>
 
 		<root-component :title="'3. Vuex'">
@@ -16,11 +17,13 @@
 <script>
 import Component from './../../hoc/Component.vue'
 import Routes from './components/Routes.vue'
+import Resource from './components/Resource.vue'
 
 export default {
 	components: {
 		rootComponent: Component,
 		appRoutes: Routes,
+		appResource: Resource
 	}
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 import Vue from 'vue'
 import Vuelidate from 'vuelidate'
 import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
 import router from './routes' // импорт файла роутов
 
 import App from './App.vue'
 
 import colorDirective from './directives/colorDirective'
+
+// Поключаем библиотеку VueResource
+Vue.use(VueResource)
 
 // Поключаем библиотеку Vuelidate
 Vue.use(Vuelidate)
